13a5976f7948125b8da03054bb78bfc4dc25c876
Alfredo Fiebig <afiebig@linuxpuertomontt.cl>
2011-04-27 23:48:14 -0300

    JSON Querry added.

--- src/aefc/QrScanner/QrScaner.java
+++ src/aefc/QrScanner/QrScaner.java
@@ -1,5 +1,23 @@
 package aefc.QrScanner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import la.droid.qr.services.Services;
 import aefc.QrScanner.R;
 import android.app.Activity;
@@ -7,6 +25,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -21,13 +40,11 @@ public class QrScaner extends Activity {
 	private String hashRut;
 	private static final int ACTIVITY_RESULT_QR_DRDROID = 0;
 
-	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
 		//ALERT DIALOG
-
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setMessage( getString(R.string.warning) )
 		.setCancelable(true)
@@ -38,7 +55,69 @@ public class QrScaner extends Activity {
 		});
 		builder.create().show();
 
+		//Get Places Data from server with JSON
+		String result = "";
 
+		//Initialization stuffs
+		InputStream is = new InputStream() {
+
+			public int read() throws IOException {
+				return 0;
+			}
+		};; ;
+
+		//the year data to send
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		nameValuePairs.add(new BasicNameValuePair("year","1980"));
+
+		//http post
+		try{
+			HttpClient httpclient = new DefaultHttpClient();
+			//TODO: Set te addres from the server service.
+			HttpPost httppost = new HttpPost("http://example.com/getAllPeopleBornAfter.php");
+			//httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			is = entity.getContent();
+		}catch(Exception e){
+			Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+
+
+		//convert response to string
+		try{
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+
+			result=sb.toString();
+		}catch(Exception e){
+			Log.e("log_tag", "Error converting result "+e.toString());
+		}
+
+		//TODO: MODIFIE THIS PART
+		//parse json data 
+		try{
+			JSONArray jArray = new JSONArray(result);
+			for(int i=0;i<jArray.length();i++){
+				JSONObject json_data = jArray.getJSONObject(i);
+				Log.i("log_tag","id: "+json_data.getInt("id")+
+						", name: "+json_data.getString("name")+
+						", sex: "+json_data.getInt("sex")+
+						", birthyear: "+json_data.getInt("birthyear")
+				);
+			}
+		}
+		catch(JSONException e){
+			Log.e("log_tag", "Error parsing data "+e.toString());
+		}
+
+		//TODO: ARRANGE DATA IN A DROPDOWN
+		
 		//"CallScanner" button
 		final Button button = (Button) findViewById(R.id.CallScanner);
 
